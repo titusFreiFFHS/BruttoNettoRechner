65ca0be52f7a52caedcce9590ac8029ffced172c
Titus Frei <titus.frei@students.ffhs.ch>
2018-02-02 15:14:50 +0100

    Initial commit

---
+++ app/src/main/java/esa/freis/ch/brutto_netto_rechner/MainActivity.java
@@ -0,0 +1,13 @@
+package esa.freis.ch.brutto_netto_rechner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
